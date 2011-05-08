ba1edd2d8b2eebb7f06992eb6940dee94d20bf07
Haacked <haacked@gmail.com>
2011-05-08 08:32:59 -0700

    Converted site to use Razor and MVC 3

--- Lmbtfy.Web/Controllers/HomeController.cs
+++ Lmbtfy.Web/Controllers/HomeController.cs
@@ -4,44 +4,36 @@ using System.Net;
 using System.Web.Mvc;
 using Lmbtfy.Web.Models;
 
-namespace Lmbtfy.Web.Controllers
-{
+namespace Lmbtfy.Web.Controllers {
     [HandleError]
-    public class HomeController : Controller
-    {
-        public ActionResult About()
-        {
+    public class HomeController : Controller {
+        public ActionResult About() {
             return View();
         }
 
-        public ActionResult Index(string q)
-        {
-            if(string.IsNullOrEmpty(q))
-            {
+        public ActionResult Index(string q) {
+            if (string.IsNullOrEmpty(q)) {
                 return View();
             }
             return View("BingThis", (object)q);
         }
 
-        public ActionResult GenerateUrl(string q)
-        {
-            string virtualPath = Url.RouteUrl("Search", new {q, Controller = "Home", Action="Index"});
+        public ActionResult GenerateUrl(string q) {
+            string virtualPath = Url.RouteUrl("Search", new { q, Controller = "Home", Action = "Index" });
             string url = Url.ToPublicUrl(virtualPath);
             string tinyUrl = GenerateTinyUrl(url);
 
-            return PartialView("GenerateUrl", new GeneratedUrlModel{Url = url, TinyUrl = tinyUrl });
+            return PartialView("_GenerateUrl", new GeneratedUrlModel { Url = url, TinyUrl = tinyUrl });
         }
 
-        protected string GenerateTinyUrl(string realUrl)
-        {
+        protected string GenerateTinyUrl(string realUrl) {
             // prepare the web page we will be asking for
             var request = (HttpWebRequest)WebRequest.Create("http://tinyurl.com/api-create.php?url=" + realUrl);
 
             // execute the request
             var response = (HttpWebResponse)request.GetResponse();
 
-            using (var sr = new StreamReader(response.GetResponseStream()))
-            {
+            using (var sr = new StreamReader(response.GetResponseStream())) {
                 return sr.ReadToEnd();
             }
         }
