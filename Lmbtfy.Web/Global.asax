90e3ba0752a66dd68c384053704eb0b590a4e70b
Haacked <haacked@gmail.com>
2011-05-08 20:54:50 -0700

    Going back to using Global.asax

--- Lmbtfy.Web/Global.asax
+++ Lmbtfy.Web/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lmbtfy.Web.Global" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lmbtfy.Web.MvcApplication" Language="C#" %>
--- Lmbtfy.Web/Global.asax.cs
+++ Lmbtfy.Web/Global.asax.cs
@@ -30,7 +30,7 @@ namespace Lmbtfy.Web {
             );
         }
 
-        public void Start() {
+        protected void Application_Start() {
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
         }
