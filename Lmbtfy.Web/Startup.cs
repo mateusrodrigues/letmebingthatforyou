d3cdfab7bb8d2c9072f32c76ad19bd5405e17991
Mateus Rodrigues de Morais <mateus@mateus.tech>
2019-11-08 15:47:28 -0300

    Update to Core 3.0

--- Lmbtfy.Web/Startup.cs
+++ Lmbtfy.Web/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Lmbtfy.Web.Services;
+﻿using Lmbtfy.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Lmbtfy.Web
 {
@@ -22,14 +18,14 @@ namespace Lmbtfy.Web
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllersWithViews();
+            services.AddRazorPages();
 
             services.AddTransient<IDirectoryService, DirectoryService>();
             services.AddTransient<IImageRepository, ImageRepository>();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -43,25 +39,11 @@ namespace Lmbtfy.Web
             app.UseStaticFiles();
 
             app.UseHttpsRedirection();
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    "Search",
-                    "",
-                    new { controller = "Home", action = "Index" }
-                );
-
-                routes.MapRoute(
-                    "OldSearch",
-                    "{predicate}",
-                    new { controller = "Home", action = "Index", predicate = "" }
-                );
-
-                routes.MapRoute(
-                    "Default",
-                    "{controller}/{action}",
-                    new { action = "Index" }
-                );
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
