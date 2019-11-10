614859a08d3935602e3e4e1acf2e093ad8da7129
Mateus Rodrigues de Morais <mateus@mateus.tech>
2019-11-10 00:04:19 -0300

    Link generation modernized and fixed

--- Lmbtfy.Web/Controllers/HomeController.cs
+++ Lmbtfy.Web/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using Lmbtfy.Web.Extensions;
-using Lmbtfy.Web.Models;
+﻿using Lmbtfy.Web.Models;
 using Lmbtfy.Web.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -35,11 +34,10 @@ namespace Lmbtfy.Web.Controllers
 
         public ActionResult GenerateUrl(string q)
         {
-            string virtualPath = Url.RouteUrl("Search", new { q, Controller = "Home", Action = "Index" });
-            string url = Url.ToPublicUrl(HttpContext, virtualPath);
-            string tinyUrl = GenerateTinyUrl(url);
+            string path = Url.ActionLink("Index", "Home", new { q }, "https", Request.Host.Value);
+            string tinyUrl = GenerateTinyUrl(path);
 
-            return PartialView("_GenerateUrl", new GeneratedUrlModel { Url = url, TinyUrl = tinyUrl });
+            return PartialView("_GenerateUrl", new GeneratedUrlModel { Url = path, TinyUrl = tinyUrl });
         }
 
         protected string GenerateTinyUrl(string realUrl)
@@ -55,28 +53,6 @@ namespace Lmbtfy.Web.Controllers
             {
                 return sr.ReadToEnd();
             }
-        }
-    }
-
-    public static class Helpers
-    {
-        public static string ToPublicUrl(this IUrlHelper urlHelper, HttpContext httpContext, string relativeUri)
-        {
-            var uriBuilder = new UriBuilder
-            {
-                Host = httpContext.Request.PathBase,
-                Path = "/",
-                Port = 80,
-                Scheme = "http",
-            };
-
-            // TODO: Commented out
-            //if (httpContext.Request.IsLocal)
-            //{
-            //    uriBuilder.Port = httpContext.Request.Url.Port;
-            //}
-
-            return new Uri(uriBuilder.Uri, relativeUri).AbsoluteUri;
         }
     }
 }
